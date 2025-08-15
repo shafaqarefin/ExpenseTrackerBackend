a18418a4981a4fc0b07a5bf5617c7967944851c4
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-08-15 23:38:45 +0600

    set up cookie parser,cors,middlwares for auth route

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
+//Importing Necessary Libraries
+require('dotenv').config({ quiet: true });
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const connectDB = require('./config/database');
+const { restrictToLoggedInUser } = require('./middlewares/authentication');
+
+//connecting to mongoDB
+connectDB()
+
+// Importing Routes
+const authRoutes = require('./routes/authRoute');
+// const userRoutes = require('./routes/userRoutes');
+
+//Declaring necessary variables
 const app = express();
+const PORT = process.env.PORT || 8000;
 
-const PORT = process.env.PORT || 5000;
+// Middleware
+app.use(express.json());        // Parse JSON requests
+app.use(cookieParser());        // Parse cookies
+app.use(cors({                  // Optional: allow my frontend to call API
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.get('/test',restrictToLoggedInUser,(req,res)=>{
+  res.status(200).send('bal')
+})
 
 
-
+// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
