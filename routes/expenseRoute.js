c73fd664d867c8a6b6ee02179662b3fbae0e9328
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-08-16 01:42:58 +0600

    seperatd get,post and patch delete into seperate routes

--- routes/expenseRoute.js
+++ routes/expenseRoute.js
@@ -1,15 +1,31 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {getExpense,createExpense}=require('../controllers/expenseController')
-const { body } = require('express-validator');
-const { validateRequest } = require('../middlewares/validation');
+const {
+  getExpense,
+  createExpense,
+  updateExpense,
+  deleteExpense,
+} = require("../controllers/expenseController");
+const { body } = require("express-validator");
+const { validateRequest } = require("../middlewares/validation");
 
+router
+  .route("/")
+  .post(
+    [
+      body("title")
+        .isLength({ min: 3 })
+        .withMessage("Title must be at least 3 characters"),
+      body("amount")
+        .isFloat({ gt: 0 })
+        .withMessage("Amount must be greater than 0"),
+      body("date").isISO8601().toDate().withMessage("Invalid date format"),
+    ],
+    validateRequest,
+    createExpense
+  )
+  .get(getExpense);
 
-router.route('/').post([
-  body('title').isLength({ min: 3 }).withMessage('Title must be at least 3 characters'),
-  body('amount').isFloat({ gt: 0 }).withMessage('Amount must be greater than 0'),
-  body('date').isISO8601().toDate().withMessage('Invalid date format')
-],validateRequest,createExpense)
+router.route("/:expenseId").patch(updateExpense).delete(deleteExpense);
 
-
-module.exports=router
+module.exports = router;
