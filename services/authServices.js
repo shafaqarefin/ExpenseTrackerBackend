df9f96759d2777783cd0a107ee6b7788d1042586
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-08-16 02:49:57 +0600

    removed unecessary comments

--- services/authServices.js
+++ services/authServices.js
@@ -1,22 +1,20 @@
-const jwt = require('jsonwebtoken');
-
+const jwt = require("jsonwebtoken");
 
 const generateUserToken = (user) => {
   return jwt.sign(
     { id: user._id, name: user.name, isAdmin: user.isAdmin }, // payload
-    process.env.JWT_SECRET,                                    // secret
-    { expiresIn: '7d' }                                       // expiration
+    process.env.JWT_SECRET, // secret
+    { expiresIn: "7d" } // expiration
   );
 };
-
 
 const getUserFromToken = (token) => {
   try {
     if (!token) return null;
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    return decoded; // { id, name, isAdmin, iat, exp }
+    const loggedInUser = jwt.verify(token, process.env.JWT_SECRET);
+    return loggedInUser;
   } catch (error) {
-    console.error('JWT verification failed:', error.message);
+    console.error("JWT verification failed:", error.message);
     return null;
   }
 };
